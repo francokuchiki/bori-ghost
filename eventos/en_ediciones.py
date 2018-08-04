a03617b88efeb18faae20b64cf4490c7e016ce32
Franco <francokuchiki@gmail.com>
2018-08-04 17:48:28 -0300

    Esperando con ansias que esto lo solucione todo.

--- eventos/en_ediciones.py
+++ eventos/en_ediciones.py
@@ -17,15 +17,10 @@ async def editar_destacados(client, antes, despues):
 		mensaje_a_editar = await client.get_message(channel, ids_destaque[i])
 		if antes.content != despues.content:
 			mensaje_a_editar.embeds[0]['description'] = despues.content
-		embed = discord.Embed.from_data(mensaje_a_editar.embeds[0])
-		if 'thumbnail' in mensaje_a_editar.embeds[0]:
-			embed.set_thumbnail(url=mensaje_a_editar.embeds[0]['thumbnail']['url'])
-		if 'image' in mensaje_a_editar.embeds[0]:
-			embed.set_image(url=mensaje_a_editar.embeds[0]['image']['url'])
-		if 'footer' in mensaje_a_editar.embeds[0]:
-			embed.set_footer(icon_url=mensaje_a_editar.embeds[0]['footer']['icon_url'],
-								text=mensaje_a_editar.embeds[0]['footer']['text'])
-		if antes.embeds != despues.embeds:
+			embed = discord.Embed.from_data(mensaje_a_editar.embeds[0])
+		elif antes.embeds != despues.embeds:
+			mensaje_a_editar.embeds[0]['description'] = antes.content
+			embed = discord.Embed.from_data(mensaje_a_editar.embeds[0])
 			if 'author' in despues.embeds[0]:
 				if 'url' in despues.embeds[0]['author']:
 					embed.description += "\n\n[**"+despues.embeds[0]['author']['name']+"**]("+\
@@ -43,4 +38,12 @@ async def editar_destacados(client, antes, despues):
 				embed.set_image(url=despues.embeds[0]['image']['url'])
 			if 'footer' in despues.embeds[0]:
 				embed.add_field(name="Footer", value=despues.embeds[0]['footer']['text'])
+			mensaje_a_editar.embeds[0]['description'] =
+		if 'thumbnail' in mensaje_a_editar.embeds[0]:
+			embed.set_thumbnail(url=mensaje_a_editar.embeds[0]['thumbnail']['url'])
+		if 'image' in mensaje_a_editar.embeds[0]:
+			embed.set_image(url=mensaje_a_editar.embeds[0]['image']['url'])
+		if 'footer' in mensaje_a_editar.embeds[0]:
+			embed.set_footer(icon_url=mensaje_a_editar.embeds[0]['footer']['icon_url'],
+								text=mensaje_a_editar.embeds[0]['footer']['text'])
 		await client.edit_message(mensaje_a_editar, embed=embed)
