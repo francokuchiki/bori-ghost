2c098fee3d11c5245b7910712b86d38b0dc94947
Franco <francokuchiki@gmail.com>
2018-08-08 17:43:22 -0300

    Cambios en confiables.

--- comandos/confiables.py
+++ comandos/confiables.py
@@ -10,19 +10,37 @@ async def confiables(client, message, nick_autor, avatar_autor, mensaje_separado
 		base_de_datos = psycopg2.connect(BD_URL, sslmode='require')
 		bd = base_de_datos.cursor()
 		confiables = get_confiables()
-		for miembro in message.mentions:
-			confiable_rol = get_confiable_role(miembro.server.roles)
-			if miembro.id in confiables:
-				bd.execute(quita_confiable, (miembro.id,))
-				await client.remove_roles(miembro, confiable_rol)
-				await client.send_message(message.channel, "Ahora {} ya no es un usuario confiable.".format(miembro.display_name))
+		confiable_rol = get_confiable_role(miembro.server.roles)
+		error_faltan = "Chico, déjame seguir con mi jueguito de Star Wars, tienes que mencionar usuarios para hacer eso, {}."
+		if len(mensaje_separado) > 1 + len(message.mentions):
+			if message.mentions != None:
+				for miembro in message.mentions:
+					if miembro.id in confiables:
+						bd.execute(quita_confiable, (miembro.id,))
+						await client.remove_roles(miembro, confiable_rol)
+						await client.send_message(message.channel, "Ahora {} ya no es un usuario confiable.".format(miembro.display_name))
+					else:
+						bd.execute(nuevo_confiable, (miembro.id,))
+						await client.add_roles(miembro, confiable_rol)
+						await client.send_message(message.channel, "Ahora {} es un usuario confiable.".format(miembro.display_name))
 			else:
-				bd.execute(nuevo_confiable, (miembro.id,))
-				await client.add_roles(miembro, confiable_rol)
-				await client.send_message(message.channel, "Ahora {} es un usuario confiable.".format(miembro.display_name))
-		else:
-			error_faltan = "Chico, déjame seguir con mi jueguito de Star Wars, tienes que mencionar usuarios para hacer eso, {}."
-			await client.send_message(message.channel, error_faltan.format(message.author.display_name))
+				await client.send_message(message.channel, error_faltan.format(message.author.display_name))
+		elif mensaje_separado[1] in {"agrega", "pon", "add", "put", "a"}:
+			if message.mentions != None:
+				for miembro in message.mentions:
+					bd.execute(nuevo_confiable, (miembro.id,))
+					await client.add_roles(miembro, confiable_rol)
+					await client.send_message(message.channel, "{} fue añadido a la lista de confiables.".format(miembro.display_name))
+			else:
+				await client.send_message(message.channel, error_faltan.format(message.author.display_name))
+		elif mensaje_separado[1] in {"quita", "saca", "remove", "delete", "del", "rm", "q"}:
+			if message.mentions != None:
+				for miembro in message.mentions:
+					bd.execute(quita_confiable, (miembro.id,))
+					await client.remove_roles(miembro, confiable_rol)
+					await client.send_message(message.channel, "{} fue quitado de la lista de confiables.".format(miembro.display_name))
+			else:
+				await client.send_message(message.channel, error_faltan.format(message.author.display_name))
 		bd.close()
 		base_de_datos.commit()
 		base_de_datos.close()
