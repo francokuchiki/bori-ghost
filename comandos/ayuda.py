43ac438005bab93fd24be0be6aedc18bb8b11e4f
Franco <francokuchiki@gmail.com>
2018-08-09 20:09:55 -0300

    Ayuda.

--- comandos/ayuda.py
+++ comandos/ayuda.py
@@ -13,7 +13,7 @@ async def ayuda_general(client, message, nick_autor, avatar_autor, mensaje_separ
 		for sub_key in comandos_ayuda[key]:
 			valor += str(mod_i)+"."+str(sub_i)+". "+sub_key+"\n"
 			for i in range(len(comandos_ayuda[key][sub_key])):
-				valor += "	"+str(mod_i)+"."+str(i+1)+". "+comandos_ayuda[key][sub_key][i]+"\n"
+				valor += "	"+str(mod_i)+"."+str(sub_i)+"."+str(i+1)+". "+comandos_ayuda[key][sub_key][i]+"\n"
 			sub_i += 1
 		embed.add_field(name=str(mod_i)+". "+key,
 						value=valor)
