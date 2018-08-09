0a1ec17ca677432300d5e1f5811d7dab5865003f
Franco <francokuchiki@gmail.com>
2018-08-09 20:07:19 -0300

    Arreglo.

--- comandos/ayuda.py
+++ comandos/ayuda.py
@@ -14,9 +14,9 @@ async def ayuda_general(client, message, nick_autor, avatar_autor, mensaje_separ
 			valor += str(mod_i)+"."+str(sub_i)+". "+sub_key+"\n"
 			for i in range(len(comandos_ayuda[key][sub_key])):
 				valor += "	"+str(mod_i)+"."+str(i+1)+". "+comandos_ayuda[key][sub_key][i]+"\n"
+			sub_i += 1
 		embed.add_field(name=str(mod_i)+". "+key,
 						value=valor)
-			sub_i += 1
 		mod_i += 1
 	embed.set_thumbnail(url=client.user.avatar_url)
 	embed.set_footer(icon_url=avatar_autor,
