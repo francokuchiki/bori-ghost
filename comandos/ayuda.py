b06f71bdfa581c5eb61358b76a3edb095fb79e51
Franco <francokuchiki@gmail.com>
2018-08-09 20:03:33 -0300

    Arreglos.

--- comandos/ayuda.py
+++ comandos/ayuda.py
@@ -5,15 +5,18 @@ from variables import ayuda, comandos_ayuda
 async def ayuda_general(client, message, nick_autor, avatar_autor, mensaje_separado, prefijo):
 	embed = discord.Embed(title="**BORI GHOST** Mensaje de Ayuda",
 							description=ayuda.format(client.user.mention, prefijo),
-							colour=0xEEEEEE)
+							colour=0xAAAAAA)
 	mod_i = 1
 	for key in comandos_ayuda:
 		valor = ""
+		sub_i = 1
 		for sub_key in comandos_ayuda[key]:
+			valor += str(mod_i)+"."+str(sub_i)+". "+comandos_ayuda[key][sub_key]+"\n"
 			for i in range(len(comandos_ayuda[key][sub_key])):
-				valor += str(mod_i)+"."+str(i+1)+". "+comandos_ayuda[key][sub_key][i]+"\n"
-		embed.add_field(name=str(mod_i)+key,
+				valor += "	"+str(mod_i)+"."+str(i+1)+". "+comandos_ayuda[key][sub_key][i]+"\n"
+		embed.add_field(name=str(mod_i)+". "+key,
 						value=valor)
+		mod_i += 1
 	embed.set_thumbnail(url=client.user.avatar_url)
 	embed.set_footer(icon_url=avatar_autor,
 					text="Este mensaje ha sido solicitado por {} ({}#{})".format(nick_autor,
