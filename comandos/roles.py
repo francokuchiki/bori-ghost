12c9b1b6d41afacc41b25abbe2826a1362b9cd7a
Franco <francokuchiki@gmail.com>
2018-08-10 15:22:22 -0300

    Arreglito.

--- comandos/roles.py
+++ comandos/roles.py
@@ -10,7 +10,7 @@ async def toggle_roles(client, message, nick_autor, avatar_autor, mensaje_separa
 			rol = ""
 			Rol = None
 			while i < len(mensaje_separado):
-				if mensaje_separado[i] in message.role_mentions:
+				if mensaje_separado[i] in message.role_mentions[0].mention:
 					Rol = discord.utils.get(message.server.roles, mention = mensaje_separado[i])
 					i = len(mensaje_separado)
 				else:
