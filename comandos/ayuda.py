406880f53858e7c6391dc153ec6647df1b008196
Franco <francokuchiki@gmail.com>
2018-08-11 17:49:53 -0300

    Emojiiiis.

--- comandos/ayuda.py
+++ comandos/ayuda.py
@@ -59,7 +59,7 @@ async def ayuda_especifica(client, message, elemento, nick_autor, avatar_autor, 
 		descripcion += "\n**__Sintaxis__**\n"+"```{}```".format(elemento.sintaxis.format(prefijo))
 		descripcion += "\n Los símbolos < y > y todo lo que esté entre paréntesis **no** debe escribirse."
 		descripcion += "\n**__Ejemplo__**\n"+"```{}```".format(elemento.ejemplo.format(prefijo))
-	embed = discord.Embed(title="Ayuda: "+elemento.nombre,
+	embed = discord.Embed(title=u"\U0001F3A9"+" Ayuda: "+elemento.nombre,
 						description=descripcion,
 						colour=0x2464CC)
 	for i in range(len(elemento.subs)):
@@ -67,7 +67,7 @@ async def ayuda_especifica(client, message, elemento, nick_autor, avatar_autor, 
 		if len(elemento.subs[i].subs) != 0:
 			for indice in range(len(elemento.subs[i].subs)):
 				valor += "——"+str(i+1)+"."+str(indice+1)+". "+elemento.subs[i].subs[indice].nombre+"\n"
-		embed.add_field(name=str(i+1)+". "+elemento.subs[i].nombre,
+		embed.add_field(name=u"\U0001F47B"+" "+str(i+1)+". "+elemento.subs[i].nombre,
 						value=valor)
 	embed.set_thumbnail(url=client.user.avatar_url)
 	embed.set_footer(icon_url=avatar_autor,
@@ -77,14 +77,14 @@ async def ayuda_especifica(client, message, elemento, nick_autor, avatar_autor, 
 		await client.send_typing(message.author)
 		await client.send_message(message.author, embed=embed)
 		await client.send_typing(message.channel)
-		await client.send_message(message.channel, "Ayuda enviada por mensaje privado.")
+		await client.send_message(message.channel, u"\U0001F4E8"+" Revisa tu MAC, súbdito. Te he enviado un mensaje privado.")
 	except discord.errors.Forbidden:
 		await client.send_typing(message.channel)
 		await client.send_message(message.channel, embed=embed)
 
 
 async def ayuda_general(client, message, nick_autor, avatar_autor, prefijo):
-	embed = discord.Embed(title="BORI GHOST: Mensaje de Ayuda",
+	embed = discord.Embed(title=u"\U0001F916"+" BORI GHOST: Mensaje de Ayuda",
 							description=ayuda.format(client.user.mention, prefijo, prefijo, prefijo),
 							colour=0x2464CC)
 	for mod_i in range(len(descripciones_ayuda)):
