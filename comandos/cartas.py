83b2f18f7bca9f7e5af1d895809296530c829c8a
Franco <francokuchiki@gmail.com>
2018-08-15 20:49:03 -0300

    Arreglito.

--- comandos/cartas.py
+++ comandos/cartas.py
@@ -32,8 +32,8 @@ async def da_carta(client, message, nick_autor, avatar_autor, mensaje_separado, 
 							description="Partido entre {} y {}.".format(jugadores[0].display_name, jugadores[1].display_name),
 							colour = 0x00AAAA)
 	embed.add_field(name="Mano de {}".format(jugadores[0].display_name),
-					value=reverso_emoji*3)
+					value=str(reverso_emoji)*3)
 	embed.add_field(name="Mesa",value="")
 	embed.add_field(name="Mano de {}".format(jugadores[1].display_name),
-					value=reverso_emoji*3)
+					value=str(reverso_emoji)*3)
 	await client.send_message(message.channel, embed=embed)
