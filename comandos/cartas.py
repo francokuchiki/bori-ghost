b087517d3481ca77bbecd2ecff569df3e5a4e414
Franco <francokuchiki@gmail.com>
2018-08-15 10:47:42 -0300

    Sin guión bajo

--- comandos/cartas.py
+++ comandos/cartas.py
@@ -6,7 +6,7 @@ async def da_carta(client, message, nick_autor, avatar_autor, mensaje_separado, 
 	servidor = discord.utils.get(client.servers, id = "310951366736609281")
 	mano_1 = []
 	while len(mano_1) < 3:
-		i = random.rand_int(40)
+		i = random.randint(40)
 		if baraja[i] not in mano_1:
 			mano_1.append(baraja[i])
 	mensaje = ""
