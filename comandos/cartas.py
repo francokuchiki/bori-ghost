0fd868fe08c93304b11ca9ef29c9f22606572ace
Franco <francokuchiki@gmail.com>
2018-08-15 10:34:09 -0300

    Sin guión

--- comandos/cartas.py
+++ comandos/cartas.py
@@ -3,6 +3,6 @@ from comandos.cartas_objetos import *
 
 async def da_carta(client, message, nick_autor, avatar_autor, mensaje_separado, prefijo):
 	servidor = discord.utils.get(client.servers, id = "310951366736609281")
-	e1_emoji = discord.utils.get(servidor.emojis, name="espada_1")
+	e1_emoji = discord.utils.get(servidor.emojis, name="espada1")
 	await client.send_message(message.channel, e1_emoji)
 	await client.send_message(message.author, e1_emoji)
