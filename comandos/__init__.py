0ad4ec6eee62d084e22abb106d24159b8ac36557
Franco <francokuchiki@gmail.com>
2018-08-15 10:15:54 -0300

    Empieza el truco.

--- comandos/__init__.py
+++ comandos/__init__.py
@@ -1,1 +1,2 @@
-__all__ = ["ayuda","ping", "cerrar", "silencio", "ban", "mix", "democracia", "prefijos", "nicks", "roles", "destacados", "confiables"]
+__all__ = ["ayuda", "ayuda_objetos","ping", "cerrar", "silencio", "ban", "mix", "democracia", "prefijos",
+			"nicks", "roles", "destacados", "confiables", "cartas_objetos", "cartas"]
---
+++ comandos/cartas.py
@@ -0,0 +1,6 @@
+import discord
+from cartas_objetos import *
+
+async def da_carta(client, message, nick_autor, avatar_autor, mensaje_separado, prefijo):
+	await client.send_message(message.channel, espada_1.emoji)
+	await client.send_message(message.author, espada_1.emoji)
