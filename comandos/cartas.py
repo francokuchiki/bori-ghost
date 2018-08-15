de89056ab07ae47e137d907d6e9b8f6da982bd5e
Franco <francokuchiki@gmail.com>
2018-08-15 10:45:06 -0300

    Pruebita.

--- comandos/cartas.py
+++ comandos/cartas.py
@@ -1,8 +1,17 @@
 import discord
+import random
 from comandos.cartas_objetos import *
 
 async def da_carta(client, message, nick_autor, avatar_autor, mensaje_separado, prefijo):
 	servidor = discord.utils.get(client.servers, id = "310951366736609281")
-	e1_emoji = discord.utils.get(servidor.emojis, name="espada1")
-	await client.send_message(message.channel, e1_emoji)
-	await client.send_message(message.author, e1_emoji)
+	mano_1 = []
+	while len(mano_1) < 3:
+		i = random.rand_int(40)
+		if baraja[i] not in mano_1:
+			mano_1.append(baraja[i])
+	mensaje = ""
+	for carta in mano_1:
+		emoji = discord.utils.get(servidor.emojis, name=carta.emoji)
+		mensaje += emoji
+	await client.send_message(message.channel, mensaje)
+	await client.send_message(message.author, mensaje)
