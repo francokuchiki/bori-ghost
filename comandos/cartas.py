fce250937173a6887d52ec9513856b03a2d6dd5b
Franco <francokuchiki@gmail.com>
2018-08-16 00:06:31 -0300

    Probando si puedo hacer todos los turnos con un while.

--- comandos/cartas.py
+++ comandos/cartas.py
@@ -39,12 +39,14 @@ async def da_carta(client, message, nick_autor, avatar_autor, mensaje_separado, 
 	embed.set_footer(icon_url="https://cdn.icon-icons.com/icons2/1310/PNG/512/hourglass_86336.png",
 						text="Es el turno de {}".format(jugadores[0].display_name))
 	mensaje_juego = await client.send_message(message.channel, embed=embed)
-	respuesta = None
-	while respuesta == None:
-		respuesta = await client.wait_for_message(author=jugadores[0])
-		if hasattr(respuesta.server, "id"):
-			if respuesta.server.id != message.server.id:
-				respuesta = None
-	emoji_jugada = manos[0][int(respuesta.content)-1].emoji
-	emoji_jugada = discord.utils.get(servidor.emojis, name=emoji_jugada)
-	await client.send_message(message.channel, str(emoji_jugada))
+	turnos = 1
+	while turnos < 7:
+		respuesta = None
+		while respuesta == None:
+			respuesta = await client.wait_for_message(author=jugadores[(turnos-1)%2])
+			if hasattr(respuesta.server, "id"):
+				if respuesta.server.id != message.server.id:
+					respuesta = None
+		emoji_jugada = manos[0][int(respuesta.content)-1].emoji
+		emoji_jugada = discord.utils.get(servidor.emojis, name=emoji_jugada)
+		await client.send_message(message.channel, str(emoji_jugada))
